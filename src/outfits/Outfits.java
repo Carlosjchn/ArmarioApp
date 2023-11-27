57ad24b96ea244c183bd7df200c2257a1e66b4c3
antoniogr12 <antoniogarciaroman123@gmail.com>
2023-11-27 13:51:44 +0100

    Cosas

--- src/outfits/Outfits.java
+++ src/outfits/Outfits.java
@@ -5,4 +5,7 @@ import static recursos.Recursos.*;
 
 public class Outfits {
     
+	 String[][] conjunto = new String[1][3];
+     Scanner sc = new Scanner(System.in);
+     int numConjuntos = 0;
 }
