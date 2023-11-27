1fc56a3f2f83950426020ea5b7d8242117ef2ead
Cjchn <carlosjchamorro@gmail.com>
2023-11-27 11:57:19 +0100

    Compra/venta 1.0, no opcion de compra

--- src/compraventa/CompraVenta.java
+++ src/compraventa/CompraVenta.java
@@ -4,15 +4,157 @@ import java.util.Scanner;
 import static recursos.Recursos.*;
 
 public class CompraVenta {
+    public static final String LIMPIAR_CMD_WINDOW = "\033[H\033[2J";
+    public static final String LIMPIAR_CMD_MACOS = "\u001b[2J";
+    public static void limpiarPantalla() {
+        System.out.println(LIMPIAR_CMD_MACOS);
+        System.out.println(LIMPIAR_CMD_WINDOW);
+    }
+
+    /*
+     * FUNCION ARRAY DINAMICOS.
+     * Esta funcion aumenta el tamaño de FILAS en 1 cada vez que se añade una serie
+     * de datos nuevos, el array dato[] debe ser igual al numero de COLUMNAS de
+     * array[][].
+     */
+    public static String[][] arrayString2D(String[][] array, String dato[]) {
+        if (array[0][0] == null) {
+            for (int k = 0; k < dato.length; k++) {
+                array[0][k] = dato[k];
+            }
+            return array;
+        } else {
+            String dinamico[][] = new String[array.length + 1][array[0].length];
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[0].length; j++) {
+                    dinamico[i][j] = array[i][j];
+                }
+            }
+            for (int k = 0; k < dato.length; k++) {
+                dinamico[dinamico.length - 1][k] = dato[k];
+            }
+            return dinamico;
+        }
+    }
+
+    public static String[][] restarPrenda(String[][] array, String dato[]) {   
+            String dinamico[][] = new String[array.length - 1][array[0].length];
+            for (int i = 0; i < dinamico.length; i++) {
+                for (int j = 0; j < dinamico[0].length; j++) {
+                    if(dato[j]!=array[i][j]){
+                        dinamico[i][j] = array[i][j];
+                    }
+                }
+            }
+            return dinamico;
+    }
+
+    public static String menu(Scanner sc) {
+        System.out.println("Opciones: \n \t 1 - Armario. \n\t 2 - Vender \n\t 3 - Comprar. \n\t 4 - Salir.");
+        String opcion = sc.nextLine();
+        return opcion;
+    }
+
+    /*
+     * FUNCIONES PARA OPCIONES APLICACIÓN.
+     * funcion para imprimir Ropa User y Ropa a la venta
+     */
+    public static void verRopa(String RopaUser[][]) {
+        for (int i = 0; i < RopaUser.length; i++) {
+            System.out.print("\n" + (i + 1) + " ");
+            for (int j = 0; j < RopaUser[0].length; j++) {
+                System.out.print(RopaUser[i][j] + " ");
+            }
+        }
+        System.out.println("");
+    }
+
+    // Funcion para poner a la venta ropa.
+    public static String[][] venderRopa(Scanner sc, String ropaUser[][], String ventaUser[][]) {
+        String prendaVender[] = new String[3];
+        System.out.println("¿Que articulo quieres vender?");
+        verRopa(ropaUser);
+        int articulo = sc.nextInt();
+        if (articulo <= ropaUser.length) {
+            for (int i = 0; i < ropaUser[0].length; i++) {
+                prendaVender[i] = ropaUser[articulo - 1][i];
+            }
+            ventaUser = arrayString2D(ventaUser, prendaVender);
+            System.out.println("¿Que precio quieres ponerle a tu " + ropaUser[articulo - 1][0] + " "
+                    + ropaUser[articulo - 1][1] + "?");
+            String precio = sc.next();
+            ventaUser[ventaUser.length - 1][ventaUser[0].length - 1] = precio;
+           
+        }
+        return ventaUser;
+    }
+
     public static void main(String[] args) {
-    /*Necesito que Login==true, RopaUser[][]
-        Funcion para ver la ropa del user.
-        Funcion para poner RopaUser a la venta con precio.
-        Funcion para ver RopaUser a la venta.
-        Funcion para ver ropa que puedes comprar.
-        Funcion para comprar y que se añada a RopaUser.
-        Funcion para mensajes con vendedor/comprador.
-        Funcion para favoritos/carrito compra.
-    */
+        /*
+         * Necesito que Login==true, RopaUser[][]
+         * Funcion para ver la ropa del user. HECHO
+         * Funcion para poner RopaUser a la venta con precio. HECHO
+         * Funcion para ver RopaUser a la venta. HECHO
+         * Funcion para ver ropa que puedes comprar.
+         * Funcion para comprar y que se añada a RopaUser.
+         * Funcion para mensajes con vendedor/comprador.
+         * Funcion para favoritos/carrito compra.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        String ropaUser[][] = { { "Sudadera", "Roja", "L" },
+                { "Camiseta", "Blanca", "M" },
+                { "Camiseta", "Negra", "L" },
+                { "Pantalon", "Cargo Negro", "L" },
+                { "Sudadera", "Negra", "L" } };
+        String ropaVenta[][] = new String[1][4];
+        boolean salir = false;
+        String opcion;
+
+        do {
+            limpiarPantalla();
+            switch (menu(sc)) {
+                case "1", "Armario", "armario":
+                    limpiarPantalla();
+                    verRopa(ropaUser);
+                    sc.nextLine();
+                    break;
+                case "2", "Vender", "vender":
+                    limpiarPantalla();
+                    System.out.println("MENÚ VENTAS: \n\t 1 - Vender ropa \n\t 2 - Ropa en venta.");
+                    opcion = sc.nextLine();
+                    switch (opcion) {
+                        case "1", "vender", "Vender":
+                            boolean vender = false;
+                            while (vender == false) {
+                                limpiarPantalla();
+                                ropaVenta = venderRopa(sc, ropaUser, ropaVenta);
+                                System.out.println("¿Quieres seguir vendiendo? (Si/No)");
+                                opcion = sc.next();
+                                if (opcion.equals("si") || opcion.equals("Si") || opcion.equals("SI")) {
+                                    vender = false;
+                                }else{
+                                    vender = true;
+                                }
+                            }
+                            break;
+                        case "2":
+                            limpiarPantalla();
+                            verRopa(ropaVenta);
+                            sc.nextLine();
+                            break;
+                    }
+                    break;
+                case "3", "Comprar", "comprar":
+                    break;
+                case "4", "Salir", "salir":
+                    System.out.println("Has cerrado la aplicación.");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no valida...");
+            }
+        } while (salir == false);
+
     }
 }
--- src/recursos/Recursos.java
+++ src/recursos/Recursos.java
@@ -1,18 +1,21 @@
 package recursos;
 
 public class Recursos {
-    //Aclaración: array[FILAS][COLUMNAS]
-    //Esta función aumenta el tamaño del array cada vez que se añade 1 dato nuevo.
+    // Aclaración: array[FILAS][COLUMNAS]
+    // Esta función aumenta el tamaño del array cada vez que se añade 1 dato nuevo.
     public static int[] arrayInt1D(int[] array, int dato) {
         int dinamico[] = new int[array.length + 1];
         int i;
         for (i = 0; i < array.length; i++) {
             dinamico[i] = array[i];
         }
-        dinamico[dinamico.length-1] = dato;
+        dinamico[dinamico.length - 1] = dato;
         return dinamico;
     }
-    //Esta funcion aumenta el tamaño de FILAS en 1 cada vez que se añade una serie de datos nuevos, el array dato[] debe ser igual al numero de COLUMNAS de array[][].
+
+    // Esta funcion aumenta el tamaño de FILAS en 1 cada vez que se añade una serie
+    // de datos nuevos, el array dato[] debe ser igual al numero de COLUMNAS de
+    // array[][].
     public static int[][] arrayInt2D(int[][] array, int dato[]) {
         int dinamico[][] = new int[array.length + 1][array[0].length];
         int i, j;
@@ -26,30 +29,38 @@ public class Recursos {
         }
         return dinamico;
     }
-    //Esta función aumenta el tamaño del array cada vez que se añade 1 dato nuevo.
+
+    // Esta función aumenta el tamaño del array cada vez que se añade 1 dato nuevo.
     public static String[] arrayString1D(String[] array, String dato) {
         String dinamico[] = new String[array.length + 1];
         for (int i = 0; i < array.length; i++) {
             dinamico[i] = array[i];
         }
-        dinamico[dinamico.length-1] = dato;
+        dinamico[dinamico.length - 1] = dato;
         return dinamico;
     }
-    //Esta funcion aumenta el tamaño de FILAS en 1 cada vez que se añade una serie de datos nuevos, el array dato[] debe ser igual al numero de COLUMNAS de array[][].
+
+    // Esta funcion aumenta el tamaño de FILAS en 1 cada vez que se añade una serie
+    // de datos nuevos, el array dato[] debe ser igual al numero de COLUMNAS de
+    // array[][].
     public static String[][] arrayString2D(String[][] array, String dato[]) {
-        String dinamico[][] = new String[array.length + 1][array[0].length];
-        int i, j;
-        for (i = 0; i < array.length; i++) {
-            for (j = 0; j < array[0].length; j++) {
-                dinamico[i][j] = array[i][j];
+        if (array[0][0] == null) {
+            for (int k = 0; k < dato.length; k++) {
+                array[0][k] = dato[k];
             }
+            return array;
+        } else {
+            String dinamico[][] = new String[array.length + 1][array[0].length];
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[0].length; j++) {
+                    dinamico[i][j] = array[i][j];
+                }
+            }
+            for (int k = 0; k < dinamico[0].length; k++) {
+                dinamico[dinamico.length - 1][k] = dato[k];
+            }
+            return dinamico;
         }
-
-        for (int k = 0; k < dinamico[0].length; k++) {
-            dinamico[dinamico.length - 1][k] = dato[k];
-        }
-
-        return dinamico;
     }
 
 }
