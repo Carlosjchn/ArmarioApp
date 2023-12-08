e85bdcc584f43c52420aed0b6f19374b2dd8adcf
José Carlos Membrive <josecarlos.s349109@cesurformacion.com>
2023-12-08 18:53:47 +0100

    08/12/23

--- src/registrouser/RegistroUser.java
+++ src/registrouser/RegistroUser.java
@@ -3,18 +3,35 @@ import java.util.Scanner;
 public class RegistroUser {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-    	System.out.println("Bienvenido a tu ArmarioApp \n" +
-    			"1. Iniciar Sesion \n" +
-    			"2. Crear cuenta \n " +
-    			"Elija 1 si ya tiene una cuenta de ArmarioApp o 2 si desea crear una nueva cuenta: ");
+    	System.out.println("Bienvenido a tu ArmarioApp, registre sus datos e inicie sesión: \n" +
+    					   " 1. Crear cuenta \n" +
+    					   " 2. Inicio de sesión \n ");
     	
-    	int opcion = sc.nextInt();
-   
-    	if (opcion==1) {
-    		    		   		
-    		 String matriz [] [] = new String [2] [2];
-             for (int i=0; i<matriz.length; i++){
-             
+    	int opcion = sc.nextInt();    	
+    	String datos [] = new String [3];
+    		if (opcion==1) {		   		        
+    			System.out.println("Introduzca su nombre: ");
+                	String nombre = sc.next();
+                	datos [0] = nombre;
+            
+                System.out.println("Introduzca su contraseña: ");
+                	String contraseña = sc.next();
+                	datos [1] = contraseña;
+        
+                System.out.println("Introduzca su correo electrónico: ");
+                	String correo = sc.next();
+                	datos [2] = correo;		  		
+              	}
+
+    		for (int i =0; i< datos.length;i++) {
+    		System.out.println(datos[i]);
+    		}
+    	
+    	
+    		if (opcion==2) {
+    			String matriz [] [] = new String [2] [2];
+    			for (int i=0; i<matriz.length; i++){
+          
                  System.out.println("Introduzca nombre de Usuario: ");
                  String nombre = sc.nextLine();
                  System.out.println("Introduzca Contraseña: ");
@@ -25,38 +42,11 @@ public class RegistroUser {
                  
                  if (matriz [i][0]==matriz [i][1]){
                      System.out.println("enhorabuena has iniciado sesion");
-                 }
-
-             }
-    		
-    		
-    	} 
-    	
-    	if (opcion==2) {
-    		
-    		String almacenarDatos [] = new String [3];
-    		
-    		System.out.println("Pon un nombre: ");
-    		String datoNombre = sc.next();
-    		almacenarDatos [0] = datoNombre;
-
-    		System.out.println("Pon una contraseña: ");
-    		String datoContraseña = sc.next();
-    		almacenarDatos [1] = datoContraseña;
-
-    		System.out.println("Pon un correo: ");
-    		String datoCorreo = sc.next();
-    		almacenarDatos [2] = datoCorreo;
-
-    		String  crearUsuario[] = almacenarDatos;
-    	
-    		
-    		for (int i = 0; i< crearUsuario.length; i++)
-    			System.out.println(crearUsuario[i]);
-       	}
 	
+                 }
+    			}
+    	   }
 	}
-       
 	public static String [] rellenarDatos (Scanner sc) {
 
         String datos [] = new String [3];
@@ -81,11 +71,6 @@ public class RegistroUser {
     	String dato = sc.next();
     	return dato;
     }
-      
-   /* public static String [][] guardarDatos (int campos, int datos, Scanner sc1) {
-    	Scanner sc1 = new Scanner (System.in);
-     	String nombre = sc1.nextLine();
-    	String matriz [][] = new String [campos] [datos];
-    	for (int i = 0; i<campos; i++) {	
-    */ 	
-	}
+             }
+             
+ 
